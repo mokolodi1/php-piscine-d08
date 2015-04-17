e378df7113923272154f6e1ed8d3e7d9fb65b320
Teo Fleming <tfleming@student.42.fr>
2015-04-17 23:02:22 +0200

    changed OnScreen: moving things up from Ship

--- ex00/OnScreen.class.php
+++ ex00/OnScreen.class.php
@@ -3,13 +3,15 @@
  class OnScreen {
 
 	# position_x/y is the top left corner
+	protected $name;
 	protected $position_x;
 	protected $position_y;
 	protected $width;
 	protected $height;
 
 	public function __construct( array $kwargs ) {
-		if ( !isset( $kwargs['x'] )
+		if ( !isset( $kwargs['name'] ) 
+				|| !isset( $kwargs['x'] )
 				|| !isset( $kwargs['y'] )
 				|| !isset( $kwargs['width'] )
 				|| !isset( $kwargs['height'] ) ) {
@@ -17,6 +19,7 @@
 						. PHP_EOL );
 			exit(1);
 		}
+		$this->name = $kwargs['name'];
 		$this->position_x = $kwargs['x'];
 		$this->position_y = $kwargs['y'];
 		$this->width = $kwargs['width'];
@@ -31,6 +34,7 @@
 				&& $y < $this->position_y + $this->height);
 	}
 
+	public function getName() {			return $this->name; 		}
 	public function getPositionX() {	return $this->position_x;	}
 	public function getPositionY() {	return $this->position_y;	}
 	public function getWidth() {		return $this->width;		}
